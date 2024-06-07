8297b6a33cb3df7aa5db8daff5fe2a7e52a1aefc
mcakyerima <mcakyerima@gmail.com>
2024-06-07 23:58:24 +0100

    updated filter

--- app/(routes)/category/[categoryId]/components/mobile-filter.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filter.tsx
@@ -3,17 +3,45 @@
 import { useState } from 'react';
 import { cn } from "@/lib/utils";
 import Button from "@/components/button";
+import { Color, Size } from "@/types/types";
+import { PlusCircle, X } from 'lucide-react';
+import { Dialog, DialogPanel } from "@headlessui/react";
+import IconButton from "@/components/ui/icon-button";
 
-const MobileFilters = () => {
-    const filter = {}
+interface MobileFiltersProps {
+    sizes: Size[];
+    colors: Color[]
+}
+
+const MobileFilters: React.FC<MobileFiltersProps> = ({
+    sizes,
+    colors,
+}) => {
+    const [ open , setOpen ] = useState(false);
+
+   const onOpen = () => setOpen(true);
+   const onClose = () => setOpen(false);
     return (
-        <div>
-            <h3 className="text-lg font-semibold" >
-                Mobile filter
-            </h3>
-            <hr className="my-4"/>
-             
-        </div>
+        <>
+            <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden py-3 mb-3">
+                Fitlers
+                <PlusCircle/>
+            </Button>
+
+            <Dialog open={open} as="div" className="relative z-40 lg:hidden" onClose={onClose}>
+                {/* background */}
+                <div className="fixed inset-0 bg-black bg-opacity-25"/>
+
+                {/* dialog position */}
+                <div className="fixed inset-0 z-40 flex">
+                    <DialogPanel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-5 shadow-xl">
+                        <div className="flex items-center justify-end px-4">
+                            <IconButton icon={<X size={15}/>} onClick={onClose}/>
+                        </div>
+                    </DialogPanel>
+                </div>
+            </Dialog>
+        </>
     )
 }
 
